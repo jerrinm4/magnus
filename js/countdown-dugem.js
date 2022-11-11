2e732b81673026cabafd0c91057860644c56a4d8
jerrinm4 <jerrinm4@gmail.com>
2022-11-11 09:53:34 +0530

    V1

--- js/countdown-dugem.js
+++ js/countdown-dugem.js
@@ -14,10 +14,10 @@
              let now = new Date().getTime(),
                  distance = countDown - now;
 
-             document.getElementById("days").innerText = Math.floor(distance / (day)),
-                 document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                 document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                 document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+             document.getElementById("days").innerHTML = Math.floor(distance / (day))+"&nbsp;&nbsp;";
+                 document.getElementById("hours").innerHTML = Math.floor((distance % (day)) / (hour))+"&nbsp;&nbsp;";
+                 document.getElementById("minutes").innerHTML = Math.floor((distance % (hour)) / (minute))+"&nbsp;&nbsp;";
+                 document.getElementById("seconds").innerHTML = Math.floor((distance % (minute)) / second);
 
              //do something later when date is reached
              if (distance < 0) {
