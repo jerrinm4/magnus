ac45780f9ddfff1b02c620ff0d24baec615ab78c
jerrinm4 <jerrinm4@gmail.com>
2022-11-15 20:05:33 +0530

    V2

--- img/blur.py
+++ img/blur.py
@@ -1,15 +1,17 @@
 import os
 from PIL import Image, ImageFilter
-d,dbl='events',"events_blur"
+d,dbl=os.path.join(os.getcwd(),'events'),os.path.join(os.getcwd(),"events_blur")
+r=15
 def resize(file):
-    image = Image.open(d+'/'+file)
+    image = Image.open(os.path.join(d,file))
+    print(image.size)
     image = image.convert('RGBA')
-    image = image.resize((image.size[0]//15,image.size[1]//15),Image.ANTIALIAS)
+    image = image.resize((image.size[0]//r,image.size[1]//15),Image.ANTIALIAS)
     image = image.filter(ImageFilter.GaussianBlur(radius = 5))
-    image.save(dbl+'/'+file,optimize=True,quality=95)
+    image.save(os.path.join(dbl,file),optimize=True,quality=95)
 
 
 files = os.listdir(d)
 for f in files:
-    if os.path.isfile(d+'/'+f):
+    if os.path.isfile(os.path.join(d,f)):
         resize(f)
